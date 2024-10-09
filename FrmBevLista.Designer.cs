f06466d8f97e0acfcdb7f4e74666c3ee92cf96c1
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-10-09 09:01:28 +0200

    updt

--- FrmBevLista.Designer.cs
+++ FrmBevLista.Designer.cs
@@ -28,20 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txbUjTetel = new TextBox();
+            btnHozzaadas = new Button();
+            btnOsszesTorlese = new Button();
+            btnKijeloltTorlese = new Button();
+            lsbTetelek = new ListBox();
             SuspendLayout();
+            // 
+            // txbUjTetel
+            // 
+            txbUjTetel.Location = new Point(12, 27);
+            txbUjTetel.Name = "txbUjTetel";
+            txbUjTetel.PlaceholderText = "írd be az új tétel nevét ide...";
+            txbUjTetel.Size = new Size(386, 32);
+            txbUjTetel.TabIndex = 0;
+            // 
+            // btnHozzaadas
+            // 
+            btnHozzaadas.Enabled = false;
+            btnHozzaadas.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            btnHozzaadas.Location = new Point(243, 89);
+            btnHozzaadas.Name = "btnHozzaadas";
+            btnHozzaadas.Size = new Size(155, 47);
+            btnHozzaadas.TabIndex = 1;
+            btnHozzaadas.Text = "Hozzáad";
+            btnHozzaadas.UseVisualStyleBackColor = true;
+            // 
+            // btnOsszesTorlese
+            // 
+            btnOsszesTorlese.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            btnOsszesTorlese.Location = new Point(243, 244);
+            btnOsszesTorlese.Name = "btnOsszesTorlese";
+            btnOsszesTorlese.Size = new Size(155, 96);
+            btnOsszesTorlese.TabIndex = 1;
+            btnOsszesTorlese.Text = "Összes törlése";
+            btnOsszesTorlese.UseVisualStyleBackColor = true;
+            // 
+            // btnKijeloltTorlese
+            // 
+            btnKijeloltTorlese.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            btnKijeloltTorlese.Location = new Point(243, 142);
+            btnKijeloltTorlese.Name = "btnKijeloltTorlese";
+            btnKijeloltTorlese.Size = new Size(155, 96);
+            btnKijeloltTorlese.TabIndex = 1;
+            btnKijeloltTorlese.Text = "Kijelölt törlése";
+            btnKijeloltTorlese.UseVisualStyleBackColor = true;
+            // 
+            // lsbTetelek
+            // 
+            lsbTetelek.FormattingEnabled = true;
+            lsbTetelek.ItemHeight = 25;
+            lsbTetelek.Items.AddRange(new object[] { "Alma", "Körte", "Banán" });
+            lsbTetelek.Location = new Point(12, 89);
+            lsbTetelek.Name = "lsbTetelek";
+            lsbTetelek.SelectionMode = SelectionMode.MultiExtended;
+            lsbTetelek.Size = new Size(225, 454);
+            lsbTetelek.TabIndex = 2;
             // 
             // FrmBevLista
             // 
             AutoScaleDimensions = new SizeF(11F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(410, 555);
+            Controls.Add(lsbTetelek);
+            Controls.Add(btnKijeloltTorlese);
+            Controls.Add(btnOsszesTorlese);
+            Controls.Add(btnHozzaadas);
+            Controls.Add(txbUjTetel);
             Font = new Font("Segoe UI", 14F);
-            Margin = new Padding(5, 5, 5, 5);
+            Margin = new Padding(5);
             Name = "FrmBevLista";
             Text = "Bevásárlólista";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txbUjTetel;
+        private Button btnHozzaadas;
+        private Button btnOsszesTorlese;
+        private Button btnKijeloltTorlese;
+        private ListBox lsbTetelek;
     }
 }
--- FrmBevLista.cs
+++ FrmBevLista.cs
@@ -5,6 +5,31 @@ namespace WFA24100901
         public FrmBevLista()
         {
             InitializeComponent();
+            txbUjTetel.TextChanged += TxbUjTetel_TextChanged;
+            btnHozzaadas.Click += BtnHozzaadas_Click;
+            btnOsszesTorlese.Click += BtnOsszesTorlese_Click;
+            btnKijeloltTorlese.Click += BtnKijeloltTorlese_Click;
+        }
+
+        private void BtnKijeloltTorlese_Click(object? sender, EventArgs e)
+        {
+            while (lsbTetelek.SelectedItems.Count > 0)
+                lsbTetelek.Items.Remove(lsbTetelek.SelectedItems[0]);
+        }
+
+        private void BtnOsszesTorlese_Click(object? sender, EventArgs e)
+        {
+            lsbTetelek.Items.Clear();
+        }
+
+        private void BtnHozzaadas_Click(object? sender, EventArgs e)
+        {
+            lsbTetelek.Items.Add(txbUjTetel.Text);
+        }
+
+        private void TxbUjTetel_TextChanged(object? sender, EventArgs e)
+        {
+            btnHozzaadas.Enabled = !string.IsNullOrWhiteSpace(txbUjTetel.Text);
         }
     }
 }
